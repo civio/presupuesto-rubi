3bec4f516d8ac10ba53f44e86d43428f98ceaca0
David Cabo <david.cabo@gmail.com>
2015-05-22 13:17:43 +0200

    Include financial data in overview page

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ BUDGET_LOADER = 'RubiBudgetLoader'
 FEATURED_PROGRAMMES = ['1710', '3321', '3261', '1630', '2410', '3263', '3340', '3370', '3410', '4411', '9242']
 
 OVERVIEW_INCOME_NODES = [['11', '113'], '13', '42', '43', '45', '46', '30', '33', '34', '39']
-OVERVIEW_EXPENSE_NODES = ['23', '92', '13', '16', '15', '33', '32', '43', '44', '91', '93', '17']
+OVERVIEW_EXPENSE_NODES = ['23', '92', '13', '16', '15', '01', '33', '32', '43', '44', '91', '93', '17']
 
 # Show an extra tab with institutional breakdown. Default: True.
 SHOW_INSTITUTIONAL_TAB = False
@@ -21,6 +21,9 @@ SHOW_INSTITUTIONAL_TAB = False
 # Show an extra column with actual revenues/expenses. Default: True.
 # Warning: the execution data still gets shown in the summary chart and in downloads.
 #SHOW_ACTUAL = True
+
+# Show financial expenditure/income in overview charts. Default: False.
+SHOW_FINANCIAL_DATA_IN_OVERVIEW = True
 
 # Search in entity names. Default: True.
 SEARCH_ENTITIES = False
