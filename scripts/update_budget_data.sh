202fdafa7f8312ca282313f3e7dc23a72a45de19
David Cabo <david@civio.es>
2015-06-04 20:53:16 +0200

    Create script to update budget data via cron

---
+++ scripts/update_budget_data.sh
@@ -0,0 +1,14 @@
+# To be scheduled via cron like:
+# 00 03 * * * cd /var/www/rubi.dondevanmisimpuestos.es/public/presupuesto-rubi/scripts && ./update_budget_data.sh
+
+python fetch_execution_data.py
+
+python fetch_payment_data.py
+
+python ../../manage.py load_budget 2015 --status=D --language=ca
+python ../../manage.py load_budget 2015 --status=D --language=es-es
+
+python ../../manage.py load_payments 2015 --language=ca
+python ../../manage.py load_payments 2015 --language=es-es
+
+sudo /home/david/clean-tmp-folder.sh rubi
