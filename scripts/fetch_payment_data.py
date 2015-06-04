f2bc7165e0996f593b63291d519d129c8b5abc0f
David Cabo <david.cabo@gmail.com>
2015-06-04 19:48:04 +0200

    Download payment data in both spanish and catalan

--- scripts/fetch_payment_data.py
+++ scripts/fetch_payment_data.py
@@ -8,7 +8,7 @@ import datetime
 
 # Read functional categories from given file and return as a dictionary
 def read_functional_categories(filename):
-  reader = csv.reader(open(functional_categories_file, 'rb'))
+  reader = csv.reader(open(filename, 'rb'))
   categories = {}
   for index, line in enumerate(reader):
     if re.match("^#", line[0]):     # Ignore comments
@@ -29,8 +29,11 @@ def read_functional_categories(filename):
 base_path = os.path.dirname(os.path.realpath(__file__))
 
 # Read functional categories data, to cross reference with payments
-functional_categories_file = os.path.join(base_path, '..', 'data', 'ca', 'areas_funcionales.csv')
-functional_categories = read_functional_categories(functional_categories_file)
+functional_categories_file_ca = os.path.join(base_path, '..', 'data', 'ca', 'areas_funcionales.csv')
+functional_categories_ca = read_functional_categories(functional_categories_file_ca)
+
+functional_categories_file_es = os.path.join(base_path, '..', 'data', 'es-es', 'areas_funcionales.csv')
+functional_categories_es = read_functional_categories(functional_categories_file_es)
 
 # Download payment data
 print "Descargando datos de pagos..."
@@ -41,14 +44,13 @@ print "Parseando datos..."
 reader = csv.reader(response)
 # Note: the output is the same for both languages in this case (not for payments for example)
 writer_ca = csv.writer(open(os.path.join(base_path, '..', 'data', 'ca', 'municipio', '2015', 'pagos.csv'), 'wb'))
-# writer_es = csv.writer(open(os.path.join(base_path, '..', 'data', 'es-es', 'municipio', '2015', 'ejecucion_ingresos.csv'), 'wb'))
+writer_es = csv.writer(open(os.path.join(base_path, '..', 'data', 'es-es', 'municipio', '2015', 'pagos.csv'), 'wb'))
 for index, line in enumerate(reader):
   if index==0:          # Ignore header line
       continue
 
   # Parse (and transform) the payment item
   programme_id = line[11]
-  area = functional_categories[programme_id]
   economic_id = line[12]
   beneficiary_id = line[2]
   description = line[4]
@@ -64,21 +66,39 @@ for index, line in enumerate(reader):
 
   # If we have no beneficiary it's because of personal data protection laws. Make a note about it
   if line[3]!='':
-    beneficiary_name = line[3]
+    beneficiary_name_ca = line[3]
+    beneficiary_name_es = line[3]
   else:
-    beneficiary_name = "<abbr title='Aquest concepte recull les persones físiques, la identitat " \
-                        "de les quals queda protegida en compliment de la Llei Orgànica de Protecció " \
-                        "de Dades.'>[Anonimitzat]*</abbr>"
+    beneficiary_name_ca = "<abbr title='Aquest concepte recull les persones físiques, la identitat " \
+                          "de les quals queda protegida en compliment de la Llei Orgànica de Protecció " \
+                          "de Dades.'>[Anonimitzat]*</abbr>"
+    beneficiary_name_es = "<abbr title='Este concepto recoge las personas físicas cuya identidad " \
+                          "queda protegida en cumplimiento de la Ley Organica de Protección de Datos.'>" \
+                          "[Anonimizado]*</abbr>"
 
-  # Output line
+  # Output lines
   writer_ca.writerow([
-      area,
+      functional_categories_ca[programme_id],
       programme_id,
       economic_id,
       '',               # period, for historical reasons, ignore
       formatted_date,
       beneficiary_id,
-      beneficiary_name,
+      beneficiary_name_ca,
+      '',               # contract type, for historical reasons, ignore
+      description,
+      amount,
+      ''                # data source, for historical reasons, ignore
+    ])
+
+  writer_es.writerow([
+      functional_categories_es[programme_id],
+      programme_id,
+      economic_id,
+      '',               # period, for historical reasons, ignore
+      formatted_date,
+      beneficiary_id,
+      beneficiary_name_es,
       '',               # contract type, for historical reasons, ignore
       description,
       amount,
@@ -86,5 +106,5 @@ for index, line in enumerate(reader):
     ])
 
 
-# # Done
-# print u"Datos descargados con éxito"
+# Done
+print u"Datos descargados con éxito"
